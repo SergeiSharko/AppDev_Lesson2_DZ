8f79020f8be4f7196914d7d696b25180f83f7699
Sergei_Sharko <vulfer_d@mail.ru>
2024-09-20 13:03:26 +0300

    Добавьте файлы проекта.

---
+++ Bits.cs
@@ -0,0 +1,30 @@
+﻿namespace Lesson2_DZ
+{
+    internal class Bits
+    {
+        private long value;
+
+        // Неявное приведение из long
+        public static implicit operator Bits(long v)
+        {
+            return new Bits { value = v };
+        }
+
+        // Неявное приведение из int
+        public static implicit operator Bits(int v)
+        {
+            return new Bits { value = v };
+        }
+
+        // Неявное приведение из byte
+        public static implicit operator Bits(byte v)
+        {
+            return new Bits { value = v };
+        }
+
+        public override string ToString()
+        {
+            return value.ToString();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Lesson2_DZ
+{
+    //Реализуйте операторы неявного приведения из long,int,byte в Bits.
+ 
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Bits bitsFromLong = 1234567890123456789L;
+            Bits bitsFromInt = 123456789;
+            Bits bitsFromByte = 123;
+
+            Console.WriteLine(bitsFromLong);
+            Console.WriteLine(bitsFromInt); 
+            Console.WriteLine(bitsFromByte);
+        }
+    }
+}
